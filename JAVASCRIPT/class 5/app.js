49a5c6432e4913b3589c55b4d5d5d9f71644dd0a
SheikhAhmer <sheikhahmerali75@gmail.com>
2022-04-17 16:46:17 +0500

    17 april 2022

---
+++ JAVASCRIPT/class 5/app.js
@@ -0,0 +1,13 @@
+// var fName = prompt("enter ur first name ?");
+// alert(fName.toUpperCase())
+// alert(fName.toLowerCase())
+// var text = "To be or not to be.";
+// var segIndex = text.lastIndexOf(".");
+// document.write(segIndex+"<br />")
+// document.write(text)
+// var newText = text.charAt(4)
+// document.write(newText)
+// document.write(newText)
+// var randomNumber = Math.ceil(Math.random()*100);
+// console.log(randomNumber)
+var number = Number(prompt("enter number"))
