07c8e65bee89aaaab323d7fdce222166e61e91ba
SheikhAhmer <sheikhahmerali75@gmail.com>
2022-06-12 16:33:47 +0500

    some code

---
+++ JAVASCRIPT/overthrow/app.js
@@ -0,0 +1,14 @@
+var aneeq = "my name is aneeq ";
+var num1 = 121;
+var isMale = true || false;
+alert(aneeq + "and i am a male");
+console.log(num1 + 43)
+var quest1 = prompt("what is ur age ?");
+console.log("aneeqs age is : "+quest1)
+aneeq = "my name is ahsan "
+var userIdentity = "and i am a male"
+alert(aneeq + userIdentity);
+var number1 = Number(prompt("Enter ur first desire number :"));
+var number2 = Number(prompt("Enter Ur second desire number"));
+var result = (number1+number2)
+console.log(result)
