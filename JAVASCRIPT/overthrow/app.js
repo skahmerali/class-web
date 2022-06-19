826a493ab6984c375c0deec42a3a2bbad48ae53c
SheikhAhmer <sheikhahmerali75@gmail.com>
2022-06-19 16:28:32 +0500

    some code

--- JAVASCRIPT/overthrow/app.js
+++ JAVASCRIPT/overthrow/app.js
@@ -1,14 +1,55 @@
-var aneeq = "my name is aneeq ";
-var num1 = 121;
-var isMale = true || false;
-alert(aneeq + "and i am a male");
-console.log(num1 + 43)
-var quest1 = prompt("what is ur age ?");
-console.log("aneeqs age is : "+quest1)
-aneeq = "my name is ahsan "
-var userIdentity = "and i am a male"
-alert(aneeq + userIdentity);
-var number1 = Number(prompt("Enter ur first desire number :"));
-var number2 = Number(prompt("Enter Ur second desire number"));
-var result = (number1+number2)
-console.log(result)
+function abc(){
+// var sadiq;
+// sadiq="md sadiq";
+
+
+// console.log("My name is : " + sadiq)
+// alert("My Nmae is : " + sadiq);
+// var aneeq = "my name is aneeq ";
+
+
+
+
+
+// var num1 = 121;
+
+
+// var isMale = true || false;
+
+
+
+// alert(aneeq + "and i am a male");
+// console.log(num1 + 43);
+// document.write(sadiq);
+
+
+
+
+// var quest1 = prompt("what is Sadqiq's age ?");
+// document.write("Sadiq's ages is : "+quest1);
+// console.log("Sadiq's age is : "+quest1);
+// aneeq = "my name is ahsan "
+// var userIdentity = "and i am a male"
+// alert(aneeq + userIdentity);
+
+
+// var quest1=prompt("Enter a number :")
+// var quest2= prompt("Enter a second number :")
+// console.log(quest1+quest2)
+
+var aneeqAge = Number(prompt("Enter Aneeq age  : "));
+var sadiqAge = Number(prompt("Enter sadiq age  : "));
+var result1 = (number1 + number2)
+var result2 = (number1 - number2)
+var result3 = (number1 * number2)
+var result4 = (number1 % number2)
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(result4)
+
+
+
+
+
+}
