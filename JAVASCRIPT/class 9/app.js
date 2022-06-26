26565845544110c7ec3c663b47a174fb2b8666cb
SheikhAhmer <sheikhahmerali75@gmail.com>
2022-06-26 16:38:23 +0500

    sunday class 26 june

---
+++ JAVASCRIPT/class 9/app.js
@@ -0,0 +1,58 @@
+// var result = prompt("what is the name founder of PAkistan");
+
+// if ( result === "Quaid e azam") {
+// console.log("the name  is right")
+// }else{
+//     console.log("the name is wrong")
+// }
+// var quest1 = prompt("what is ur name ?");
+// var quest2 = prompt("what is ur Father name ?");
+// var quest3 = prompt("what is ur age ?");
+// var quest4 = prompt("what is ur class ?");
+// if(quest1==="ahmer"){
+//     console.log("welcome to ahmer")
+// }else if(quest2 === "sheikh nazim"){
+//     console.log("welcome to ahmer")
+
+// }else if(quest3==="25"){
+//     console.log("welcome to ahmer")
+
+// }else if( quest4 === "ten"){
+//     console.log("welcome to ahmer")
+
+// }else{
+//     console.log(" KON HO BHAE TM?")
+// }
+// if (quest1 === "ahmer") {
+
+//     if (quest2 === "sheikh nazim") {
+//         if (quest3 === "25") {
+//             console.log("welcome to ahmer")
+//         }
+
+
+//     }
+// } else {
+//     console.log(" KON HO BHAE TM?")
+// }
+// if (quest1 === "ahmer" && quest2 === "nazim" || quest3 === "25") {
+//     console.log("welcome to ahmer")
+// }else{
+//     console.log("not valid")
+// }
+// var lahore ="lahore";
+var cities = ["karachi","islamabad","lahore"]
+// console.log(cities)
+// cities.push(prompt("enter ur name"))
+// console.log(cities)
+// if(quest1==="ahmer"){
+//     console.log("welcome to ahmer")
+// }
+// var table=prompt("Enter a table number")
+for(var i = 1; i <= 10 ; i++) {
+// if(cities[i]==="lahore"){
+//     console.log("welcome to lahore")
+// }else{
+//     console.log("not valid")
+    document.write(2+"*"+i+"="+(2*i)+"<br>")
+}
