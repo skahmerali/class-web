46c1874433bd7ba8da4f5b47dde3a24e22c49c7d
SheikhAhmer <sheikhahmerali75@gmail.com>
2022-07-16 17:26:18 +0500

    16 july responsive class

---
+++ JAVASCRIPT/class 10/app.js
@@ -0,0 +1,27 @@
+// var std3=prompt("enter ur name ")
+// var std4=prompt("enter ur name ")
+// var students = [
+//     "ahsan", "aneeq" , std3 , std4
+// ]
+// document.write(students);
+// console.log(students);
+// students.pop()
+// console.log(students);
+// students.push("ahmer")
+// console.log(students);
+// students.splice(0,2)
+// console.log(students);
+// var table = prompt("Enter a Table number :")
+// for (var i = 1 ; i<=10; i++){
+// document.write(table + "*" + i + "=" + table*i +"<br />")
+// }
+var stars = "*"
+for(var i = 1; i <= 10; i++){
+    for(var j = 1; j <= i ; j++){
+       document.write( "*"+"<br />");
+
+    //     // document.write(stars.push("*"))
+    }
+    // console.log("*")
+    // document.write(i + "<br />")
+}
