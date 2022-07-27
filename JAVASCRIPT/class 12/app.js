5d7647d0be91f05915239b4dadefb49740078c10
SheikhAhmer <sheikhahmerali75@gmail.com>
2022-07-27 19:27:21 +0500

    27-07 (event)

---
+++ JAVASCRIPT/class 12/app.js
@@ -0,0 +1,23 @@
+// var pra1 = document.getElementById("pra1");
+// function pra (){
+//     console.log("user name : "+" "+pra1.value)
+// }
+
+
+// function pra() {
+//     var dayOfWk = document.getElementById("pra1").value;
+//     // console.log("user name : " + " " + pra1.value)
+//     switch (dayOfWk) {
+//         case "Sat":
+//             alert("Whoopee");
+//             break;
+//         case "Sun":
+//             alert("Whoopee for sun");
+//             break;
+//         case "Fri":
+//             alert("TGIF!");
+//             break;
+//         default:
+//             alert("Shoot me now!");
+//     }
+// }
